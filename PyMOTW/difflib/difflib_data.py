55e31ce2248b0bddfe9f9d90d348e97f3d5f5819
Doug Hellmann <doug.hellmann@gmail.com>
2007-09-25 23:49:19 +0000

    add difflib for release 1.21
    --HG--
    extra : convert_revision : svn%3A98f53aa3-d424-0410-b225-a548b0275c4d/PyMOTW/trunk%401068

---
+++ PyMOTW/difflib/difflib_data.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Doug Hellmann.
+#
+#
+#                         All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby
+# granted, provided that the above copyright notice appear in all
+# copies and that both that copyright notice and this permission
+# notice appear in supporting documentation, and that the name of Doug
+# Hellmann not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission.
+#
+# DOUG HELLMANN DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+# NO EVENT SHALL DOUG HELLMANN BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+"""Data for use with difflib examples.
+
+"""
+
+__version__ = "$Id$"
+
+bio1 = """Doug Hellmann is a Senior Software Engineer at Racemi.  He has been
+programming in Python since version 1.4 on a variety of Unix and
+non-Unix platforms. He has worked on projects ranging from mapping to
+medical news publishing, with a little banking thrown in for good
+measure.  He writes the popular "Python Module of the
+Week" blog series."""
+bio1_lines = bio1.splitlines()
+
+bio2 = """Doug Hellmann is a Senior Software Engineer at Racemi.  He has been
+programming in Python since version 1.4 on a variety of Unix, and
+non-Unix, platforms. He has worked on projects ranging from GIS systems to
+medical news publishing, with a little banking thrown in for good
+measure.  In addition to the popular "Python Module of the
+Week" blog series, he writes a monthly column for Python Magazine
+titled "Something Completely Different"."""
+bio2_lines = bio2.splitlines()
---
+++ PyMOTW/difflib/difflib_seq.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Doug Hellmann.
+#
+#
+#                         All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby
+# granted, provided that the above copyright notice appear in all
+# copies and that both that copyright notice and this permission
+# notice appear in supporting documentation, and that the name of Doug
+# Hellmann not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission.
+#
+# DOUG HELLMANN DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+# NO EVENT SHALL DOUG HELLMANN BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+"""SequenceMatcher example
+
+"""
+
+__version__ = "$Id$"
+
+import difflib
+from difflib_data import *
+
+s1 = [ 1, 2, 3, 5, 6, 4 ]
+s2 = [ 2, 3, 5, 4, 6, 1 ]
+
+matcher = difflib.SequenceMatcher(None, s1, s2)
+for tag, i1, i2, j1, j2 in matcher.get_opcodes(): 
+    print ("%7s s1[%d:%d] (%s) s2[%d:%d] (%s)" % 
+           (tag, i1, i2, s1[i1:i2], j1, j2, s2[j1:j2])) 
+
