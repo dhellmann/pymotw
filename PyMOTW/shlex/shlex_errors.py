bdbe771b111caeeea74e6baf031a9118861d512f
Doug Hellmann <doug.hellmann@gmail.com>
2007-10-14 22:48:32 +0000

    add shlex
    --HG--
    extra : convert_revision : svn%3A98f53aa3-d424-0410-b225-a548b0275c4d/PyMOTW/trunk%401093

---
+++ PyMOTW/shlex/shlex_errors.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Doug Hellmann.
+#
+#
+#                         All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby
+# granted, provided that the above copyright notice appear in all
+# copies and that both that copyright notice and this permission
+# notice appear in supporting documentation, and that the name of Doug
+# Hellmann not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission.
+#
+# DOUG HELLMANN DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+# NO EVENT SHALL DOUG HELLMANN BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+"""Handling parse errors.
+
+"""
+
+__version__ = "$Id$"
+
+import shlex
+
+text = """This line is ok.
+This line has an "unfinished quote.
+This line is ok, too.
+"""
+
+print 'ORIGINAL:', repr(text)
+print
+
+lexer = shlex.shlex(text)
+
+print 'TOKENS:'
+try:
+    for token in lexer:
+        print repr(token)
+except ValueError, err:
+    first_line_of_error = lexer.token.splitlines()[0]
+    print 'ERROR:', lexer.error_leader(), str(err), 'following "' + first_line_of_error + '"'
