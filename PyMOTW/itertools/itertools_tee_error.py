3d2e746095b9e05e33c9cf891aa758f7bd30b046
Doug Hellmann <doug.hellmann@gmail.com>
2007-10-20 14:57:55 +0000

    add itertools
    --HG--
    extra : convert_revision : svn%3A98f53aa3-d424-0410-b225-a548b0275c4d/PyMOTW/trunk%401102

---
+++ PyMOTW/itertools/itertools_tee_error.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Doug Hellmann.
+#
+#
+#                         All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby
+# granted, provided that the above copyright notice appear in all
+# copies and that both that copyright notice and this permission
+# notice appear in supporting documentation, and that the name of Doug
+# Hellmann not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission.
+#
+# DOUG HELLMANN DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+# NO EVENT SHALL DOUG HELLMANN BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+"""Using tee()
+
+"""
+
+__version__ = "$Id$"
+
+from itertools import *
+
+r = islice(count(), 5)
+i1, i2 = tee(r)
+
+for i in r:
+    print 'r:', i
+    if i > 1:
+        break
+
+for i in i1:
+    print 'i1:', i
+for i in i2:
+    print 'i2:', i
