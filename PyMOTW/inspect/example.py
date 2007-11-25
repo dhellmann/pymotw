a24240157ac6d4d0f345e9d32183da6764ee902e
Doug Hellmann <doug.hellmann@gmail.com>
2007-11-25 13:50:05 +0000

    copy inspect code in to prepare release
    --HG--
    extra : convert_revision : svn%3A98f53aa3-d424-0410-b225-a548b0275c4d/PyMOTW/trunk%401147

---
+++ PyMOTW/inspect/example.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+# This comment appears first
+# and spans 2 lines.
+
+# This comment does not show up in the output of getcomments().
+
+#
+# Copyright 2007 Doug Hellmann.
+#
+#
+#                         All Rights Reserved
+#
+# Permission to use, copy, modify, and distribute this software and
+# its documentation for any purpose and without fee is hereby
+# granted, provided that the above copyright notice appear in all
+# copies and that both that copyright notice and this permission
+# notice appear in supporting documentation, and that the name of Doug
+# Hellmann not be used in advertising or publicity pertaining to
+# distribution of the software without specific, written prior
+# permission.
+#
+# DOUG HELLMANN DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+# INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN
+# NO EVENT SHALL DOUG HELLMANN BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+# CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+# OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+"""Sample file to serve as the basis for inspect examples.
+"""
+
+__version__ = "$Id$"
+
+# Import system modules
+
+
+# Import local modules
+
+
+# Module
+
+def module_level_function(arg1, arg2='default', *args, **kwargs):
+    """This function is declared in the module."""
+    local_variable = arg1
+    return
+
+class A(object):
+    """The A class."""
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        "Returns the name of the instance."
+        return self.name
+
+instance_of_a = A('sample_instance')
+
+class B(A):
+    """This is the B class.
+    It is derived from A.
+    """
+
+    # This method is not part of A.
+    def do_something(self):
+        """Does some work"""
+        pass
+
+    def get_name(self):
+        "Overrides version from A"
+        return 'B(' + self.name + ')'
