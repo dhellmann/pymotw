447d96b972ac713de3b3d5fbcc5a0bd3237cc542
Doug Hellmann <doug.hellmann@gmail.com>
2008-05-04 18:28:53 +0000

    prepare for release
    --HG--
    extra : convert_revision : svn%3A98f53aa3-d424-0410-b225-a548b0275c4d/PyMOTW/trunk%401394

---
+++ PyMOTW/cmd/cmd_do_shell.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2008 Doug Hellmann All rights reserved.
+#
+"""
+"""
+
+__version__ = "$Id$"
+
+import cmd
+import os
+
+class ShellEnabled(cmd.Cmd):
+    
+    last_output = ''
+
+    def do_shell(self, line):
+        "Run a shell command"
+        print "running shell command:", line
+        output = os.popen(line).read()
+        print output
+        self.last_output = output
+    
+    def do_echo(self, line):
+        "Print the input, replacing '$out' with the output of the last shell command"
+        # Obviously not robust
+        print line.replace('$out', self.last_output)
+    
+    def do_EOF(self, line):
+        return True
+    
+if __name__ == '__main__':
+    ShellEnabled().cmdloop()
---
+++ PyMOTW/cmd/cmd_file.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2008 Doug Hellmann All rights reserved.
+#
+"""
+"""
+
+__version__ = "$Id$"
+
+import cmd
+
+class HelloWorld(cmd.Cmd):
+    """Simple command processor example."""
+    
+    # Disable rawinput module use
+    use_rawinput = False
+    
+    # Do not show a prompt after each command read
+    prompt = ''
+    
+    def do_greet(self, line):
+        print "hello,", line
+    
+    def do_EOF(self, line):
+        return True
+
+if __name__ == '__main__':
+    import sys
+    input = open(sys.argv[1], 'rt')
+    try:
+        HelloWorld(stdin=input).cmdloop()
+    finally:
+        input.close()
