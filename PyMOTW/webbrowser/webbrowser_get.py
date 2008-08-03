32c90b3cdc72df8702c400f3ef5cefee6e9eac29
Doug Hellmann <doug.hellmann@gmail.com>
2008-08-03 13:11:53 +0000

    ready for release
    --HG--
    extra : convert_revision : svn%3A98f53aa3-d424-0410-b225-a548b0275c4d/PyMOTW/trunk%401544

---
+++ PyMOTW/webbrowser/webbrowser_get.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2008 Doug Hellmann All rights reserved.
+#
+"""
+"""
+
+__version__ = "$Id$"
+
+import webbrowser
+
+b = webbrowser.get('lynx')
+b.open('http://docs.python.org/lib/module-webbrowser.html')
---
+++ PyMOTW/webbrowser/webbrowser_open_new.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2008 Doug Hellmann All rights reserved.
+#
+"""
+"""
+
+__version__ = "$Id$"
+
+import webbrowser
+
+webbrowser.open_new('http://docs.python.org/lib/module-webbrowser.html')
