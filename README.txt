b549bb8dc1cf76db5937c028a726578aa72c2183
Doug Hellmann <doug.hellmann@gmail.com>
2008-12-28 14:32:48 +0000

    convert build to paver
    --HG--
    extra : convert_revision : svn%3A98f53aa3-d424-0410-b225-a548b0275c4d/PyMOTW/trunk%401869

--- README.txt
+++ README.txt
@@ -1,11 +1,27 @@
 Python Module of the Week
--------------------------
+=========================
 
-This package includes example code from my Python Module of the Week
-blog series (http://www.doughellmann.com/PyMOTW or
-http://feeds.feedburner.com/PyMOTW)
+PyMOTW is a series of blog posts written by `Doug Hellmann <http://www.doughellmann.com/>`_.  It was started as a way to build the habit of writing something on a regular basis.  The focus of the series is building a set of example code for the modules in the `Python <http://www.python.org/>`_ standard library.
 
-The plan is for me to work my way through all of the Python standard
-library modules, writing a short post with example code for each one.
-I am releasing all of the example code in this easy to download
-collection.
+:Project Home Page: http://www.doughellmann.com/PyMOTW/
+
+Complete documentation for the standard library can be found on the Python web site at http://docs.python.org/.
+
+Subscribe
+=========
+
+As new articles are written, they are posted to `my blog <http://blog.doughellmann.com/>`_.  Updates are available by `RSS <http://feeds.feedburner.com/PyMOTW>`_ and `email <http://www.feedburner.com/fb/a/emailverifySubmit?feedId=806224&amp;loc=en_US>`_.
+
+Translations and Other Versions
+===============================
+
+`Ernesto Rico Schmidt <http://denklab.org/>`_ provides a Spanish translation that follows the English version posts. Ernesto is in Bolivia, and is translating these examples as a way to contribute to the members of the `Bolivian Free Software <http://www.softwarelibre.org.bo/>`_ community who use Python.   The full list of articles available in Spanish can be found at http://denklab.org/articles/category/pymotw/, and of course there is an `RSS feed <http://denklab.org/feeds/articles/category/pymotw/>`_.
+
+Gerard Flanagan is working on a "Python compendium" called `The Hazel Tree <http://www.thehazeltree.org/>`_.  He is converting a collection of old and new of Python-related reference material into reStructuredText and then building a single searchable repository from the results.  I am very pleased to have PyMOTW included with works from authors like Mark Pilgrim, Fredrik Lundh, Andrew Kuchling, and a growing list of others.
+
+Copyright and Licensing
+=======================
+
+All of the prose from the Python Module of the Week is licensed under a `Creative Commons Attribution, Non-commercial, Share-alike 3.0 <http://creativecommons.org/licenses/by-nc-sa/3.0/us/>`_ license.  You are free to share and create derivative works from it.  If you post the material online, you must give attribution and link to the PyMOTW home page (http://www.doughellmann.com/PyMOTW/).  You may not use this work for commercial purposes.  If you alter, transform, or build upon this work, you may distribute the resulting work only under the same or similar license to this one.
+
+The source code included here is copyright Doug Hellmann and licensed under the BSD license.
---
+++ pavement.py
@@ -0,0 +1,365 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright (c) 2008 Doug Hellmann All rights reserved.
+#
+"""
+TODO
+
+ - make sure all of the files from the manifests are included
+ 
+"""
+
+__version__ = "$Id$"
+
+# Standard library
+import os
+
+# Third-party
+import paver
+import sphinx
+
+# TODO
+# - move these variables to options?
+# - see about adding more args to sphinx runner to avoid having to write my own
+# 
+
+# What project are we building?
+PROJECT = 'PyMOTW'
+
+# What version is this? (take from path in svn tree)
+VERSION = path(os.getcwd()).name
+os.environ['VERSION'] = VERSION
+
+# What is the current module being documented?
+MODULE = path('module').text().rstrip()
+os.environ['MODULE'] = MODULE
+
+# Read the long description to give to setup
+README = path('README.txt').text()
+
+# Scan the input for package information
+# to grab any data files (text, images, etc.) 
+# associated with sub-packages.
+PACKAGE_DATA = setuputils.find_package_data(PROJECT, 
+                                            package=PROJECT,
+                                            only_in_packages=True,
+                                            )
+
+options(
+    setup=Bunch(
+        name = PROJECT,
+        version = VERSION,
+        
+        description = 'Python Module of the Week Examples: ' + MODULE,
+        long_description = README,
+
+        author = 'Doug Hellmann',
+        author_email = 'doug.hellmann@gmail.com',
+
+        url = 'http://www.doughellmann.com/PyMOTW/',
+        download_url = 'http://www.doughellmann.com/downloads/%s-%s.tar.gz' % \
+                        (PROJECT, VERSION),
+
+        classifiers = [ 'Development Status :: 5 - Production/Stable',
+                        'Environment :: Console',
+                        'Intended Audience :: Developers',
+                        'Intended Audience :: Education',
+                        'License :: OSI Approved :: BSD License',
+                        'Operating System :: POSIX',
+                        'Programming Language :: Python',
+                        'Topic :: Software Development',
+                        ],
+
+        platforms = ('Any',),
+        keywords = ('python', 'PyMOTW', 'documentation'),
+
+        # It seems wrong to have to list recursive packages explicitly.
+        packages = sorted(PACKAGE_DATA.keys()),
+        package_data=PACKAGE_DATA,
+        zip_safe=False,
+
+        ),
+    
+    sdist = Bunch(
+        outdir='~/Desktop',
+    ),
+    
+    sphinx = Bunch(
+        sourcedir=PROJECT,
+        docroot = '.',
+        builder = 'html',
+        doctrees='sphinx/doctrees',
+        confdir = 'sphinx',
+    ),
+
+    html = Bunch(
+        builddir='docs',
+        outdir='docs',
+        templates='pkg',
+    ),
+
+    website=Bunch(
+        templates = 'web',
+        #outdir = 'web',
+        builddir = 'web',
+        
+        # What server hosts the website?
+        server = 'www.doughellmann.com',
+        server_path = '/var/www/doughellmann/DocumentRoot/PyMOTW/',
+        # What template should be used for the web site HTML?
+        template_source = '~/Devel/personal/doughellmann/templates/base.html',
+        template_dest = 'sphinx/templates/web/base.html',
+    ),
+
+    pdf=Bunch(
+        templates='pkg',
+        #outdir='pdf_output',
+        builddir='web',
+        builder='latex',
+    ),
+    
+    blog=Bunch(
+        sourcedir=path(PROJECT)/MODULE,
+        builddir='blog_posts',
+        outdir='blog_posts',
+        confdir='sphinx/blog',
+        doctrees='blog_posts/doctrees',
+    ),
+
+)
+
+def remake_directories(*dirnames):
+    """Remove the directories and recreate them.
+    """
+    for d in dirnames:
+        d = path(d)
+        if d.exists():
+            d.rmtree()
+        d.mkdir()
+    return
+
+@task
+@needs(['generate_setup', 'minilib', 'html_clean', 
+        'setuptools.command.sdist'
+        ])
+def sdist():
+    """Create a source distribution.
+    """
+    # Copy the output file to the desktop
+    dist_files = path('dist').glob('*.tar.gz')
+    dest_dir = path(options.sdist.outdir).expanduser()
+    for f in dist_files:
+        f.move(dest_dir)
+    return
+
+@task
+def html_clean():
+    """Remove sphinx output directories before building the HTML.
+    """
+    remake_directories(options.sphinx.doctrees, options.html.outdir)
+    call_task('html')
+    return
+
+###########################
+# Adapted from paver source
+
+def _get_paths():
+    """look up the options that determine where all of the files are."""
+    opts = options
+    
+    docroot = path(opts.get('docroot', 'docs'))
+    if not docroot.exists():
+        raise BuildFailure("Sphinx documentation root (%s) does not exist."
+                           % docroot)
+    
+    builddir = docroot / opts.get("builddir", ".build")
+    builddir.mkdir()
+    
+    srcdir = docroot / opts.get("sourcedir", "")
+    if not srcdir.exists():
+        raise BuildFailure("Sphinx source file dir (%s) does not exist" 
+                            % srcdir)
+    
+    # Where is the sphinx conf.py file?
+    confdir = path(opts.get('confdir', srcdir))
+    
+    # Where should output files be generated?
+    outdir = opts.get('outdir', '')
+    if outdir:
+        outdir = path(outdir)
+    else:
+        outdir = builddir / opts.get('builder', 'html')
+    outdir.mkdir()
+    
+    # Where are doctrees cached?
+    doctrees = opts.get('doctrees', '')
+    if not doctrees:
+        doctrees = builddir / "doctrees"
+    else:
+        doctrees = path(doctrees)
+    doctrees.mkdir()
+
+    return Bunch(locals())
+
+def run_sphinx(*option_sets):
+    """Helper function to run sphinx with common options.
+    
+    Pass the names of namespaces to be used in the search path
+    for options.
+    """
+    if 'sphinx' not in option_sets:
+        option_sets += ('sphinx',)
+    kwds = dict(add_rest=False)
+    options.order(*option_sets, **kwds)
+    paths = _get_paths()
+    sphinxopts = ['', 
+                  '-b', options.get('builder', 'html'), 
+                  '-d', paths.doctrees, 
+                  '-c', paths.confdir,
+                  paths.srcdir, paths.outdir]
+    dry("sphinx-build %s" % (" ".join(sphinxopts),), sphinx.main, sphinxopts)
+    return
+
+@task
+def html():
+    """Build HTML documentation using Sphinx. This uses the following
+    options in a "sphinx" section of the options.
+    
+    docroot
+      the root under which Sphinx will be working.
+      default: docs
+    builddir
+      directory under the docroot where the resulting files are put.
+      default: build
+    sourcedir
+      directory under the docroot for the source files
+      default: (empty string)
+    doctrees
+      the location of the cached doctrees
+      default: $builddir/doctrees
+    confdir
+      the location of the sphinx conf.py
+      default: $sourcedir
+    outdir
+      the location of the generated output files
+      default: $builddir/$builder
+    builder
+      the name of the sphinx builder to use
+      default: html
+    """
+    os.environ['TEMPLATES'] = options.html.templates
+    run_sphinx('html')
+    return
+
+###########################
+
+@task
+def pdf():
+    """Generate the PDF book.
+    """
+    os.environ['TEMPLATES'] = options.pdf.templates
+    run_sphinx('pdf')
+    latex_dir = path(options.pdf.builddir) / 'latex'
+    sh('cd %s; make' % latex_dir)
+    return
+
+@task
+@needs([ 'webhtml', 'pdf'])
+def website():
+    """Create local copy of website files.
+    """
+    # Copy the PDF to the files to be copied to the directory to install
+    pdf_file = path(options.pdf.builddir) / 'latex' / (PROJECT + '-' + VERSION + '.pdf')
+    pdf_file.copy(path(options.website.builddir) / 'html')
+    return
+
+@task
+def installwebsite():
+    """Rebuild and copy website files to the remote server.
+    """
+    # Clean up
+    remake_directories(options.pdf.builddir, options.website.builddir)
+    # Rebuild
+    call_task('website')
+    # Copy to the server
+    os.environ['RSYNC_RSH'] = '/usr/bin/ssh'
+    src_path = path(options.website.builddir) / 'html'
+    sh('cd %s; rsync --archive --delete --verbose . %s:%s' % 
+        (src_path, options.website.server, options.website.server_path))
+    return
+
+@task
+def webtemplatebase():
+    """Import the latest version of the web page template from the source.
+    """
+    dest = path(options.website.template_dest).expanduser()
+    src = path(options.website.template_source).expanduser()
+    if not dest.exists() or (src.mtime > dest.mtime):
+        src.copy(dest)
+    return
+
+@task
+@needs(['webtemplatebase'])
+def webhtml():
+    """Generate HTML files for website.
+    """
+    os.environ['TEMPLATES'] = options.pdf.templates
+    run_sphinx('website')
+    return
+
+def clean_blog_html(body):
+    # Clean up the HTML
+    import re
+    import sys
+    from BeautifulSoup import BeautifulSoup
+    from cStringIO import StringIO
+
+    # The post body is passed to stdin.
+    soup = BeautifulSoup(body)
+
+    # Remove the permalinks to each header since the blog does not have
+    # the styles to hide them.
+    links = soup.findAll('a', attrs={'class':"headerlink"})
+    [l.extract() for l in links]
+
+    # Get BeautifulSoup's version of the string
+    s = soup.__str__(prettyPrint=False)
+
+    # Remove extra newlines.  This depends on the fact that
+    # code blocks are passed through pygments, which wraps each part of the line
+    # in a span tag.
+    pattern = re.compile(r'([^s][^p][^a][^n]>)\n$', re.DOTALL|re.IGNORECASE)
+    s = ''.join(pattern.sub(r'\1', l) for l in StringIO(s))
+    
+    return s
+
+def gen_blog_post(index_file, blog_file):
+    """Generate the blog post body.
+    """
+    # Add link to project home page at the end
+    body = index_file.text().strip()
+    output_body = clean_blog_html(body) + '''<p><a class="reference external" href="http://www.doughellmann.com/PyMOTW/">PyMOTW Home</a></p>'''
+    
+    blog_file.write_text(output_body)
+    return
+
+@task
+def blog():
+    """Generate the blog post version of the HTML for the current module.
+    """
+    # Clean and recreate output directory
+    remake_directories(options.blog.outdir)
+    outdir = path(options.blog.outdir)
+    
+    # Generate html from sphinx
+    run_sphinx('blog')
+    
+    index_file = outdir / 'index.html'
+    blog_file = outdir / 'blog.html'
+    dry("Write blog post body to %s" % blog_file, 
+        gen_blog_post, index_file=index_file, blog_file=blog_file)
+    
+    if 'EDITOR' in os.environ:
+        sh('$EDITOR %s' % blog_file)
+    return
