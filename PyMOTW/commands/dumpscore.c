23f8cb75cb65e8bbab646b77f41cd3ad215b6037
Doug Hellmann <doug.hellmann@gmail.com>
2009-11-26 13:03:27 -0500

    definitely dump the core file

--- PyMOTW/commands/dumpscore.c
+++ PyMOTW/commands/dumpscore.c
@@ -1,8 +1,18 @@
 /* Simple example app to cause a core dump. */
 
-#include "stdio.h"
+#include <stdio.h>
+#include <signal.h>
+
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 int main(int argc, char ** argv) {
-    char * p = NULL;
-    printf(p);
+
+    struct rlimit limit;
+
+    limit.rlim_cur = limit.rlim_max = RLIM_INFINITY;
+    setrlimit(RLIMIT_CORE, &limit);
+
+    return raise(SIGABRT);
 }
