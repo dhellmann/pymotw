f07a97b7c303db62e2dc509300e2bc66b7e8a228
Doug Hellmann <doug.hellmann@gmail.com>
2010-07-09 09:05:47 -0400

    bump the version number

--- pavement.py
+++ pavement.py
@@ -33,7 +33,7 @@ except:
 PROJECT = 'PyMOTW'
 
 # What version is this?
-VERSION = '1.118'
+VERSION = '1.118.1'
 
 # The sphinx templates expect the VERSION in the shell environment
 os.environ['VERSION'] = VERSION
