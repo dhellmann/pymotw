46d522cdc3fb3ff1dc01250ba78c6a33f69761c3
Doug Hellmann <doug.hellmann@gmail.com>
2010-07-11 09:15:59 -0400

    update version before release

--- pavement.py
+++ pavement.py
@@ -33,7 +33,7 @@ except:
 PROJECT = 'PyMOTW'
 
 # What version is this?
-VERSION = '1.118.1'
+VERSION = '1.119'
 
 # The sphinx templates expect the VERSION in the shell environment
 os.environ['VERSION'] = VERSION
