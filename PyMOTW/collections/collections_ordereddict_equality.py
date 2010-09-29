13e8a34c300ed7d0844b9c8738dc8b5c01339761
Doug Hellmann <doug.hellmann@gmail.com>
2010-09-29 07:41:21 -0400

    fix typo in ordereddict example

--- PyMOTW/collections/collections_ordereddict_equality.py
+++ PyMOTW/collections/collections_ordereddict_equality.py
@@ -9,7 +9,7 @@
 
 import collections
 
-print 'Regular dictionary:',
+print 'dict       :',
 d1 = {}
 d1['a'] = 'A'
 d1['b'] = 'B'
@@ -18,11 +18,11 @@ d1['d'] = 'D'
 d1['e'] = 'E'
 
 d2 = {}
-d1['e'] = 'E'
-d1['d'] = 'D'
-d1['c'] = 'C'
-d1['b'] = 'B'
-d1['a'] = 'A'
+d2['e'] = 'E'
+d2['d'] = 'D'
+d2['c'] = 'C'
+d2['b'] = 'B'
+d2['a'] = 'A'
 
 print d1 == d2
 
@@ -36,10 +36,10 @@ d1['d'] = 'D'
 d1['e'] = 'E'
 
 d2 = collections.OrderedDict()
-d1['e'] = 'E'
-d1['d'] = 'D'
-d1['c'] = 'C'
-d1['b'] = 'B'
-d1['a'] = 'A'
+d2['e'] = 'E'
+d2['d'] = 'D'
+d2['c'] = 'C'
+d2['b'] = 'B'
+d2['a'] = 'A'
 
 print d1 == d2
--- pavement.py
+++ pavement.py
@@ -34,7 +34,7 @@ except:
 PROJECT = 'PyMOTW'
 
 # What version is this?
-VERSION = '1.128'
+VERSION = '1.128.1'
 
 # The sphinx templates expect the VERSION in the shell environment
 os.environ['VERSION'] = VERSION
